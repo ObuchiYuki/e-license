f0ef70b384a04a17daf85de32ea7debdaf9a09ba
ObuchiYuki <yukibochi1@gmail.com>
2023-02-24 11:58:45 +0900

    update env

--- School/CommandConfiguration.py
+++ School/CommandConfiguration.py
@@ -1,7 +1,25 @@
+import os
+from typing import Union
+
 from dataclasses import dataclass
 
 @dataclass
 class CommandConfiguration:
-    loginurl: str
     username: str
     password: str
+    loginurl: str
+
+    @staticmethod
+    def from_env() -> Union["CommandConfiguration", None]:
+        ELICENSE_USERNAME = os.getenv("ELICENSE_USERNAME")
+        if ELICENSE_USERNAME is None: return None
+        
+        ELICENSE_PASSWORD = os.getenv("ELICENSE_PASSWORD")
+        if ELICENSE_PASSWORD is None: return None
+        
+        ELICENSE_LOGIN = os.getenv("ELICENSE_LOGIN")
+        if ELICENSE_LOGIN is None: return None
+
+        return CommandConfiguration(ELICENSE_USERNAME, ELICENSE_PASSWORD, ELICENSE_LOGIN)
+
+        
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import json
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv; load_dotenv()
+
 from Core.Catalyst import selenium_make, CatalystLoader
 from Core.Logger import Logger
 from Core.FileManager import FileManager
@@ -14,13 +16,21 @@ from School.FrameIOManager import FrameIOManager
 from School.ClassNotifier import ClassNotifier
 from School.BookTableScanner import BookTableScanner
 
+from School.CommandConfiguration import CommandConfiguration
+
 if __name__ == "__main__":
     command_name = "driveschool"
     logger = Logger(command_name, is_debug=False)
     filemanager = FileManager(command_name, Path(os.path.expanduser('~')), logger)
+
+    config = CommandConfiguration.from_env()
+    if config is None:
+        logger.error("Command configuration not found in environmental values. Set ELICENSE_USERNAME (username), ELICENSE_PASSWORD (password), ELICENSE_LOGIN (login page URL) in environmental values or .env file.")
+        exit(1)
+
     driver = selenium_make()
     loader = CatalystLoader(driver, logger)
-    login_catalyst = LoginCatalyst("23949", "KTYobuchi3", loader)
+    login_catalyst = LoginCatalyst(config, loader)
     booktable_catalyst = BookTableCatalyst(login_catalyst, loader)
     
     book_table = booktable_catalyst.run()
@@ -29,6 +39,6 @@ if __name__ == "__main__":
     scanner = BookTableScanner(frame_io, book_table)
     updated_frames = scanner.find_updated_frames()
     
-    notifier = ClassNotifier(logger)
+    notifier = ClassNotifier(config, logger)
     if len(updated_frames) > 0:
         notifier.notify(updated_frames)
